9d5491d3a8aa7d1a5cae27cc0953ae4292cb8d87
atul jaiswal <jaiswalatul018@gmai.com>
2024-05-30 23:07:10 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [beers, setBeers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.sampleapis.com/beers/ale')
+      .then(response => response.json())
+      .then(data => setBeers(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const filteredBeers = beers.filter(beer =>
+    beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Beers</h1>
+      <input
+        type="text"
+        placeholder="Search for beers"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+      <div className="beer-container">
+        {filteredBeers.map(beer => (
+          <div key={beer.id} className="beer-card">
+            <img src={beer.image} alt={beer.name} />
+            <h2>{beer.name}</h2>
+            <p>{beer.description}</p>
+            <p>Price:{beer.price}</p>
+            <p>Average:{beer.rating.average}</p>
+            <p>Reviews:{beer.rating.reviews}</p>
+            
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
